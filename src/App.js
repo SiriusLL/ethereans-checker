6f38e85671999ae2d9a4837349847fdebd5a8589
SiriusLL <sirius.love.light@gmail.com>
2021-09-06 23:28:11 -0600

    axios request for api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState();
+  // const [term, setTerm] = useState();
+  // const [name, setName] = useState([]);
+
+  useEffect(() => {
+    let num = 9045;
+    axios
+      .get(
+        `https://ipfs.io/ipfs/QmdYxMgJJS8MvMScv1AKnWkSxGGeMbPaKKoZ1nweXNTcT7/${num}`
+      )
+      .then((res) => {
+        console.log("res", res.data);
+        // setState(JSON.stringify(res.data));
+        setState(res.data);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  }, []);
+  console.log("s", state);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <div>{state && state.name}</div>
+      <div></div>
     </div>
   );
 }
